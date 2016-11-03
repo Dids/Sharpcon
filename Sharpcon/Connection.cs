a0e1804be5e12704f4636b99f7b8d3ac1e7e2b07
Pauli Jokela <pauli.jokela@didstopia.com>
2016-11-03 10:18:44 +0200

    Added simple exception handling and fixed blocked WebSocket calls by making them async.

--- Sharpcon/Connection.cs
+++ Sharpcon/Connection.cs
@@ -20,32 +20,55 @@ namespace Sharpcon
 
         #region Methods
 
-        public static string ConnectionString(string address, string port, string password) => $"ws://{address}:{port}/{password}";
+        public static string ConnectionString(string address, string port, string password)
+            => $"ws://{address}:{port}/{password}";
 
         public static void Initialise(string url)
         {
-            webSocket = new WebSocket(url);
+            try
+            {
+                webSocket = new WebSocket(url);
 
-            webSocket.OnOpen += OnOpen;
-            webSocket.OnMessage += OnMessage;
-            webSocket.OnError += OnError;
-            webSocket.OnClose += OnClose;
+                webSocket.OnOpen += OnOpen;
+                webSocket.OnMessage += OnMessage;
+                webSocket.OnError += OnError;
+                webSocket.OnClose += OnClose;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                form2?.Close();
+            }
         }
 
         public static void Connect()
         {
+            if (webSocket == null)
+            {
+                Console.WriteLine("Can't connect, WebSocket is null");
+                return;
+            }
+
+            Console.WriteLine("WebSocket connecting");
+            webSocket.ConnectAsync();
+
             form2 = new Form2();
             form2.ShowDialog();
-
-            webSocket.Connect();
         }
 
-        public static void Disconnect() => webSocket.Close();
+        public static void Disconnect() => webSocket.CloseAsync();
 
         public static void Send(string packet)
         {
-            webSocket.Send(packet);
-            MessageBox.Show("Packet sent!");
+            if (webSocket.ReadyState == WebSocketState.Open)
+            {
+                webSocket.SendAsync(packet, null);
+                Console.WriteLine("Packet sent: " + packet);
+            }
+            else
+            {
+                Console.WriteLine("WebSocket connection not ready, not sending packet");
+            }
         }
 
         #endregion
@@ -54,22 +77,22 @@ namespace Sharpcon
 
         private static void OnClose(object sender, CloseEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("WebSocket connection closed: " + e.Reason);
         }
 
         private static void OnError(object sender, ErrorEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(e.Message);
         }
 
         private static void OnMessage(object sender, MessageEventArgs e)
         {
-
+            Console.WriteLine("Packet received: " + e.Data);
         }
 
         private static void OnOpen(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("WebSocket connection opened");
         }
 
         #endregion
